107b198d31663b2723100e5b88a3a11e58285512
Olivier3456 <olivier.larrey@gmail.com>
2023-05-26 07:51:48 +0200

    Trigger for hand pointing

--- Assets/Scripts/AnimateHands.cs
+++ Assets/Scripts/AnimateHands.cs
@@ -6,24 +6,64 @@ using UnityEngine.InputSystem;
 public class AnimateHands : MonoBehaviour
 {
 
-    [SerializeField] private InputActionProperty _pinchAction;
+    //[SerializeField] private InputActionProperty _pinchAction;
 
-    [SerializeField] private InputActionProperty _gripAction;
-    
+    //[SerializeField] private InputActionProperty _gripAction;
+
     [SerializeField] private Animator _animator;
-    
-    void Update()
+
+    //void Update()
+    //{
+    //    float gripVal = _gripAction.action.ReadValue<float>();
+    //    _animator.SetFloat("Pointing", gripVal);
+
+    //}
+
+
+
+    private void OnTriggerEnter(Collider other)
     {
-        float gripVal = _gripAction.action.ReadValue<float>();
-        _animator.SetFloat("Pointing",gripVal);
-
-
-
-
-        //float triggerVal = _pinchAction.action.ReadValue<float>();
-        //_animator.SetFloat("Trigger", triggerVal);
-
-        //float gripVal = _gripAction.action.ReadValue<float>();
-        //_animator.SetFloat("Grip",gripVal);
+        if (other.CompareTag("Hand trigger"))
+        _animator.SetFloat("Pointing", 1);
     }
+
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Hand trigger"))
+            _animator.SetFloat("Pointing", 0);
+    }
+
+   
+
+
 }
+
+
+
+
+//public class AnimateHands : MonoBehaviour
+//{
+
+//    [SerializeField] private InputActionProperty _pinchAction;
+
+//    [SerializeField] private InputActionProperty _gripAction;
+    
+//    [SerializeField] private Animator _animator;
+    
+//    void Update()
+//    {
+//        float gripVal = _gripAction.action.ReadValue<float>();
+//        _animator.SetFloat("Pointing",gripVal);
+
+
+
+
+//        //float triggerVal = _pinchAction.action.ReadValue<float>();
+//        //_animator.SetFloat("Trigger", triggerVal);
+
+//        //float gripVal = _gripAction.action.ReadValue<float>();
+//        //_animator.SetFloat("Grip",gripVal);
+//    }
+//}
